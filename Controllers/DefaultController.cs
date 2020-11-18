567cd1cb484e674233d9e75d8dc4fa9dae9b8a4e
ManikandanRamSaro <mani95ram@outlook.com>
2020-11-18 22:57:31 +0530

    first commit on 18-11-2020

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StructuredMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Structured MVC page";
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StructuredMVC.MvcApplication" Language="C#" %>
